04f7fd98a9307991545cf84b7cc2fe5232e44907
barth0l0mew <barth0l0mew@mail.ru>
2022-12-23 06:25:58 +0300

    checkbox

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const store = [
+  {type:'js', title:'Выучить JavaScript', checked:true},
+  {type:'git', title:'Попрактиковаться с Git', checked:true},
+  {type:'react', title:'Изучить React', checked:false},
+  {type:'node', title:'Понять NodeJS', checked:false},
+  {type:'job', title:'Устроиться на работу', checked:false},
+  
+]
